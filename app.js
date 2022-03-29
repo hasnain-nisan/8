9b636134b86758c3145b723a364a4b10a218bbaa
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 <hasnain.nisan@outlook.com>
2022-03-29 11:37:28 +0600

    Connected to mongoDB

--- app.js
+++ app.js
@@ -6,15 +6,24 @@ const express = require('express');
 const app = express();
 const tasks = require('./routes/tasks');
 const connectDB = require('./db/connect')
+require('dotenv').config()
 
 const port = 3000
 
 // middleware
 app.use(express.json())
 
-
+//routes
 app.use('/api/v1/tasks', tasks)
 
 
-app.listen(port, console.log(`Server is listeniing on port ${port}`))
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, console.log(`Server is listeniing on port ${port}`))
+    } catch (error) {
+        console.log(error);
+    }
+}
 
+start()
--- db/connect.js
+++ db/connect.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose')
 
-const connectionString = 'mongodb+srv://Nisan:1234@nodeexpressproject.bnwgo.mongodb.net/TASK-MANAGER?retryWrites=true&w=majority'
-
 const connectDB = (url) => {
     return mongoose.connect(url)
 }
