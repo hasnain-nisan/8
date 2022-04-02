c5c6bab439d4d2f1774c395980b3d31d4cefc609
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 <hasnain.nisan@outlook.com>
2022-04-02 11:36:20 +0600

    Get all task, create task, get a single task from mongo db done

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,18 +1,35 @@
 const Task = require('../models/Task')
 
-const getAllTasks = (req, res) => {
-    res.send('All ITEMS');
+const getAllTasks = async (req, res) => {
+    try {
+        const tasks = await Task.find({})
+        res.status(201).json({tasks});
+    } catch (error) {
+        res.status(500).json({ error})
+    }
 }
 
 const createTask = async (req, res) => {
-    const task = await Task.create(req.body)
-    res.status(201).json(task);
+    try {
+        const task = await Task.create(req.body)
+        res.status(201).json(task);
+    } catch (error) {
+        res.status(500).json({ error})
+    }
 }
 
-const getTask = (req, res) => {
-    res.json({
-        "id": req.params.id
-    });
+const getTask = async (req, res) => {
+    try {
+        const task = await Task.findOne({
+            "_id": req.params.id
+        })
+        if(!task){
+            return res.status(404).json({ "error": `Task not found with id: ${req.params.id}` })
+        }
+        res.status(201).json({task});
+    } catch (error) {
+        res.status(500).json({ error})
+    }
 }
 
 const updateTask = (req, res) => {
